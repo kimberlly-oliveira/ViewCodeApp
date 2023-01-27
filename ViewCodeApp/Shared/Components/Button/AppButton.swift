7db002e537925ca55e3c7b393819922f5f11ffe6
kimberlly-oliveira <kimberlly.oliveira@hotmail.com>
2023-01-26 20:35:41 -0600

    Exercise - ViewCode: creating views only using code

--- ViewCodeApp/Shared/Components/Button/AppButton.swift
+++ ViewCodeApp/Shared/Components/Button/AppButton.swift
@@ -28,7 +28,7 @@ class AppButton: UIButton {
     }
     
     required init(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("init(coder:) has not been implemented ")
     }
     
     private func setup(){
